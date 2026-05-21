973d59abc794bc769b4077ad928af6b2dc0b3844
average <bayogjayr@gmail.com>
2026-05-22 00:26:12 +0800

    feat(coffee): minimal API endpoint with status-code mapping

--- coffee-machine/src/Api/Program.cs
+++ coffee-machine/src/Api/Program.cs
@@ -1,6 +1,26 @@
+using CoffeeMachine.Api.Models;
+using CoffeeMachine.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton(TimeProvider.System);
+builder.Services.AddSingleton<IBrewCounter, BrewCounter>();
+builder.Services.AddSingleton<IBrewService, BrewService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/brew-coffee", (IBrewService brewService) =>
+{
+    var outcome = brewService.Brew();
+    return outcome switch
+    {
+        BrewOutcome.Ready ready => Results.Ok(ready.Response),
+        BrewOutcome.OutOfCoffee => Results.StatusCode(StatusCodes.Status503ServiceUnavailable),
+        BrewOutcome.Teapot => Results.StatusCode(StatusCodes.Status418ImATeapot),
+        _ => Results.StatusCode(StatusCodes.Status500InternalServerError)
+    };
+});
 
 app.Run();
+
+public partial class Program { }
